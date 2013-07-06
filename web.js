2198025756e22087cb62f155389fbb19763cb949
Ubuntu <ubuntu@ip-10-164-65-236.ec2.internal>
2013-07-06 23:10:15 +0000

    Modified web.js

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
-var express = require('express');
+var fs = require('fs'),
+	express = require('express');
 
+var index = fs.readFileSync('index.html');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(index.toString());
 });
 
 var port = process.env.PORT || 5000;
